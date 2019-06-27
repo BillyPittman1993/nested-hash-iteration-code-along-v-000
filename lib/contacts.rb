6a16871267e281a4944320cce3402dee82b8d268
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-27 18:14:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,13 +15,11 @@ require 'pry'
    }
   
   def remove_strawberry(contacts)
-  contacts.each do |character, value| 
-    if character == "Freddy Mercury" 
-    value.each do |variable, data| 
-      if data == 
-        :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| 
-        ice_cream == "strawberry"}
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
       end
     end
